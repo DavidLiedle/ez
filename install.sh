625724f11a7abca2473dbaf65743201a0a5693ce
David Christian Liedle <david.liedle@gmail.com>
2017-03-03 12:41:57 -0500

    restructuring where some files are located

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-# Install this stuff
+# Install this repository and all its stuff
 echo 'not yet implemented'
 exit 1
+
+# Note that installation of _eZ Platform_ would be a command under tree/commands
